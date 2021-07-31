a4a797e8a8cd39c47ab79c551892fe684dbc1e02
DaveHP-Hub <davehp139@hotmail.com>
2021-07-31 15:56:17 -0500

    03 Formulario para actualizar y eliminar Mecanico Listo

--- WebMiTaller/Mecanico/VerMecanico.aspx
+++ WebMiTaller/Mecanico/VerMecanico.aspx
@@ -4,13 +4,50 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<link rel="stylesheet" href="../css/sweetalert2.min.css" />
+<script src="../js/sweetalert2.all.min.js" ></script>
+<script src="../js/index.js"></script>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <title></title>
+     <title>Ver mecanico solicitado</title>
 </head>
 <body>
-    <form id="form1" runat="server">
-        <div>
+      <nav class="navbar navbar-dark bg-dark">
+         <div class="container-fluid">
+           <a class="navbar-brand" href="../index.aspx">
+               <img src="/images/logo.png" alt="" width="110" height="60">
+           </a>
+         </div>
+     </nav>
+
+    <form id="form1" runat="server" class="container">
+        <div class="row" >
+            <div class="col-md-6 mt-4" >
+
+             <asp:Button CssClass="btn btn-danger" ID="btnBack" runat="server" Text="Regresar" OnClick="btnBack_Click"/>
+            </div>
+        </div>
+        <div class="row ">
+            <div class="col-md-6" >
+                Nombre: 
+                <asp:TextBox CssClass="form-control" ID="txtNombre" runat="server"></asp:TextBox>
+                Apellido Paterno: 
+                <asp:TextBox CssClass="form-control" ID="txtApellidoP" runat="server"></asp:TextBox>
+                Apellido Materno:
+                <asp:TextBox CssClass="form-control" ID="txtApellidoM" runat="server"></asp:TextBox>
+                Telefono: 
+                <asp:TextBox CssClass="form-control" ID="txtTelefono" runat="server"></asp:TextBox>
+                Correo: 
+                <asp:TextBox CssClass="form-control" ID="txtCorreo" runat="server"></asp:TextBox>
+            </div>
+            <div class="col-md-6" >
+                <h4>Acciones: </h4>
+                <asp:Button CssClass="btn btn-primary m-2" ID="btnUpdate" runat="server" Text="Actualizar" OnClick="btnActualizar_Click" />
+                <asp:Button CssClass="btn btn-danger m-2" ID="btnDelete" runat="server" Text="Eliminar" OnClick="btnEliminar_Click" />   
+            </div>
         </div>
     </form>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
--- WebMiTaller/Mecanico/VerMecanico.aspx.cs
+++ WebMiTaller/Mecanico/VerMecanico.aspx.cs
@@ -5,12 +5,114 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+using ClassCapaEntidades;
+using ClassLogicaNegocioTaller;
+
+
 namespace WebMiTaller.Mecanico
 {
     public partial class VerMecanico : System.Web.UI.Page
     {
+        //Referencia a la logica de negocio
+        LogicaMecanicos lgM = null;
+        //Objeto del tipo: Mecanico, a utilizar para eliminar o actualizar
+        ClassCapaEntidades.Mecanico mecha_ = null;
+        //Variable para almacenar id
+        string id_Tecnico = "";
+
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            //Validación del PostBack
+            if (IsPostBack == false)
+            {
+                lgM = new LogicaMecanicos();
+                Session["lgMC"] = lgM;
+                id_Tecnico = (string)Session["idTecnico"];
+                getClientID();
+            }
+            else
+            {
+                lgM = (LogicaMecanicos)Session["lgMC"];
+                id_Tecnico = (string)Session["idTecnico"];
+            }
+        }
+
+        //Metodo axulixar para no sobrecargar el pageLoad de mucho codigo.
+        public void getClientID()
+        {
+            //Variable local
+            string message = "";
+            mecha_ = lgM.getMechanicID(id_Tecnico, ref message);
+            txtNombre.Text = mecha_.Nombre;
+            txtApellidoP.Text = mecha_.App;
+            txtApellidoM.Text = mecha_.Apm;
+            txtTelefono.Text = mecha_.Celular;
+            txtCorreo.Text = mecha_.correo;
+        }
+
+        protected void btnBack_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("../index.aspx");
+        }
+
+        protected void btnActualizar_Click(object sender, EventArgs e)
+        {
+            //Variabls locales
+            string mssg = "";
+            Boolean flag = false;
+
+            //Creamos un objeto como axuliar para el momento en el cual se haga un actualización
+            //ó eliminación del Mecanico seleccionado
+            ClassCapaEntidades.Mecanico auxiliary = new ClassCapaEntidades.Mecanico
+            {
+                Nombre = txtNombre.Text,
+                App = txtApellidoP.Text,
+                Apm = txtApellidoM.Text,
+                Celular = txtTelefono.Text,
+                correo = txtCorreo.Text
+            };
+
+
+            //Cambiamos el status de nuestra bandera segun sea el caso ya sea verdadero o falso
+            flag = lgM.updateMechanic(auxiliary, ref mssg, id_Tecnico);
+
+            //Si la respuesta fue Verdadera(True) Regresamos a la pangina principal de Mecanicos donde veremos 
+            //Reflejado el nuevo registro en nuestro control GridView
+            if (flag)
+            {
+                Response.Redirect("Mecanico.aspx");
+            }
+            else
+            {
+                //En casso contrario mandaremos un mensaje de alerta de SweetAlert con el error cometido por el usuario
+
+                Page.ClientScript.RegisterStartupScript(this.GetType(), "ERRORUPDATE", "msgbox(`Error`, `" + mssg + "`, `error`)", true);
+            }
+
+
+
+        }
+
+        protected void btnEliminar_Click(object sender, EventArgs e)
+        {
+            //Variables locales
+            string message_ = "";
+            Boolean flag_ = false;
+
+            //Recibimos la respuesta en nuestra variable flag_
+            flag_ = lgM.deleteMechanic(id_Tecnico, ref message_);
+            //Validamos si la respuesta es verdadera, procedemos a enviar a la pagina de inicio de Mecanicos
+            //Y se vere reflejado el cambio en nuestro control GridView
+            if (flag_)
+            {
+                Response.Redirect("Mecanico.aspx");
+            }
+            else
+            {
+                //En caso contrario, mandaremos un mensaje de alerta de SweetAlert con el error cometido por el usuario
+                Page.ClientScript.RegisterStartupScript(this.GetType(), "ERRORDELETE", "msgbox(`Error`, `" + message_ + "`, `error`)", true);
+            }
 
         }
     }
